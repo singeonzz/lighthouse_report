93f9e4034d26f0c07ee62750fd4a8f224d98fc80
沈银岗 <shenyingang@chuanglintech.com>
2023-10-13 16:42:57 +0800

    Commit: 1.0.0

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # lighthouse_report
 lighthouse测试
 
-<!-- 'https://www.baidu.com' -->
 
-*** lighthouserc 中的配置 ***
-
+** lighthouserc 中的配置 **
+*** options ***
 - port?: number; // chrome的端口
 - logLevel?: 'silent' | 'error' | 'info' | 'verbose'; // 日志等级
 - output?: 'json' | 'html'| 'csv'; // 报告输出格式
@@ -16,3 +15,10 @@ lighthouse测试
 - emulatedFormFactor?: 'mobile' | 'desktop' | 'none'; // 模拟设备
 - locale?: Locale; // 语言
 
+*** configs ***
+extends: 'lighthouse:default' | undefined; // 是否继承默认配置
+settings: Object | undefined; // 运行时配置
+passes: Object[]; // 采集器
+audits: string[]; // 审查器
+categories: Object | undefined; // 类别
+groups: Object | undefined; // 分组
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
 const lighthouse = require('lighthouse');
-const { URL } = require('url');
+const {
+  URL
+} = require('url');
 const path = require('path');
 const fs = require('fs');
 
@@ -11,10 +13,9 @@ function generateReport(runnerResult) {
   const D = now.getDate();
   const H = now.getHours();
   const m = now.getMinutes();
-  const filename = `lhr-report@${Y}-${M + 1 < 10 ? '0' + (M + 1) : M + 1}-${D}-${H}-${m}.json`;
+  const filename = `lhr-report@${Y}-${M + 1 < 10 ? '0' + (M + 1) : M + 1}-${D}-${H}-${m}.html`;
   const htmlReportPath = path.join(__dirname, 'public', filename);
 
-  // 新建一个文件
   fs.writeFile(htmlReportPath, runnerResult.report, (err) => {
     if (err) {
       console.error('Failed to create file:', err);
@@ -24,24 +25,35 @@ function generateReport(runnerResult) {
   });
 
 }
-(async() => {
-    const url = 'https://huaban.com/discovery/';
-    const browser = await puppeteer.launch({
+
+(async () => {
+  const url = 'http://172.20.20.129:8084/test_all.html';
+  const browser = await puppeteer.launch({
     // 是否运行浏览器无头模式(boolean)
     headless: false,
     // 是否自动打开调试工具(boolean)，若此值为true，headless自动置为fasle
-    devtools: true,
+    devtools: false,
   });
-  try {
-    const lhr = await lighthouse(
-      url, {
-        port: (new URL(browser.wsEndpoint())).port,
-        output: 'json',
-        logLevel: 'info',
+
+  const lhr = await lighthouse(
+    url, {
+      port: (new URL(browser.wsEndpoint())).port,
+      output: 'html',
+      logLevel: 'info',
+    }, {
+      extends: 'lighthouse:default',
+      settings: {
+        formFactor: 'desktop',
+        emulatedFormFactor: 'desktop',
+        screenEmulation: { 
+          mobile: false,
+          width: 900,
+          height:900,
+        },
       }
-    );
-    generateReport(lhr)
-  } catch {}
+    }
+  );
+  generateReport(lhr)
 
   await browser.close();
 })();
