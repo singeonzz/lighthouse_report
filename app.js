7ad6c868a2b39a829249459d3399df48c5892d82
沈银岗 <shenyingang@chuanglintech.com>
2023-10-13 15:40:04 +0800

    Commit: 1.0.0

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const puppeteer = require('puppeteer');
 const lighthouse = require('lighthouse');
 const { URL } = require('url');
 const path = require('path');
-const printer = require('lighthouse/lighthouse-cli/printer');
-const Reporter = require('lighthouse/lighthouse-core/report/report-generator');
+const fs = require('fs');
+
 function generateReport(runnerResult) {
   const now = new Date();
   const Y = now.getFullYear();
@@ -11,25 +11,37 @@ function generateReport(runnerResult) {
   const D = now.getDate();
   const H = now.getHours();
   const m = now.getMinutes();
-  const filename = `lhr-report@${Y}-${M + 1 < 10 ? '0' + (M + 1) : M + 1}-${D}-${H}-${m}.html`;
+  const filename = `lhr-report@${Y}-${M + 1 < 10 ? '0' + (M + 1) : M + 1}-${D}-${H}-${m}.json`;
   const htmlReportPath = path.join(__dirname, 'public', filename);
-  const reportHtml = Reporter.generateReport(runnerResult.lhr, 'html');
-  printer.write(reportHtml, 'html', htmlReportPath);
+
+  // 新建一个文件
+  fs.writeFile(htmlReportPath, runnerResult.report, (err) => {
+    if (err) {
+      console.error('Failed to create file:', err);
+      return;
+    }
+    console.log('File created successfully!');
+  });
+
 }
 (async() => {
     const url = 'https://huaban.com/discovery/';
     const browser = await puppeteer.launch({
-      headless: false,
-      defaultViewport: null,
-    });
+    // 是否运行浏览器无头模式(boolean)
+    headless: false,
+    // 是否自动打开调试工具(boolean)，若此值为true，headless自动置为fasle
+    devtools: true,
+  });
+  try {
     const lhr = await lighthouse(
-      url,
-      {
+      url, {
         port: (new URL(browser.wsEndpoint())).port,
         output: 'json',
         logLevel: 'info',
       }
     );
     generateReport(lhr)
-    await browser.close();
+  } catch {}
+
+  await browser.close();
 })();
